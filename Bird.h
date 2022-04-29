43693383672e5fb9220f00ed1a3ee327c98fff76
Nick Larson <57819852+nickcoding2057@users.noreply.github.com>
2022-04-29 16:48:09 -0700

    added bird class

---
+++ Bird.h
@@ -0,0 +1,42 @@
+// This is the basic class for the birds
+// x and y are coordinates
+// hp is how many hits it takes to kill
+// value is how many points it is worth
+// isHit tells if the incoming shot landed
+
+class Bird{
+    public:
+    Bird();
+    Bird(float a, float b);
+    bool isHit(float a, float b);
+    float x, y, hp, value;
+}
+
+Bird::Bird()
+{
+    x = y = 100;
+    hp = 1;
+    value = 5;
+}
+
+// Non-default constructor for creating a bird with a x,y coordinate
+Bird::Bird(float a, float b)
+{
+    x = a;
+    y = b;
+    hp = 1;
+    value = 5;
+}
+
+bool Bird::isHit(float a, float b)
+{
+    if (x == a && y == b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
