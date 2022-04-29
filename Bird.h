f9442d3cdd76f9a8c82ae40368435a14cf374f5d
Nick Larson <57819852+nickcoding2057@users.noreply.github.com>
2022-04-29 16:58:18 -0700

    added trajectory

--- Bird.h
+++ Bird.h
@@ -4,16 +4,22 @@
 // value is how many points it is worth
 // isHit tells if the incoming shot landed
 
+using namespace std;
+#include <stdlib.h>
+
 class Bird{
     public:
     Bird();
     Bird(float a, float b);
     bool isHit(float a, float b);
-    float x, y, hp, value;
+    float x, y, hp, value, trajX, trajY;
+    void updateTrajectory(x += trajX; y += trajY;);
 }
 
 Bird::Bird()
 {
+    trajX = rand() % 100;
+    trajY = rand() % 100;
     x = y = 100;
     hp = 1;
     value = 5;
